b353515ffbfa944ccc939e47752e3be69d37094a
Harish Bhalaa <107175621+harish-bhalaa-07@users.noreply.github.com>
2023-11-19 21:00:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, jsonify
-import os
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle
 
-app = Flask(__name__)
+app=Flask(__name__)
+model=pickle.load(open('clgadminequity.pkl','rb'))
+
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return render_template('index5.html')  #html page here
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route('/predict5',methods=['POST'])
+def predict5():
+    
+    int_features=[[int(x) for x in request.form.values()]]
+    print(int_features)
+    final_features=np.array(int_features)  
+    prediction=model.predict(final_features)
+    prediction=prediction.tolist()
+    
+    
+    data = {
+           
+            "You are selected at college of " : prediction
+        }
+    return jsonify(data)
+    
+    # return render_template('index.html',prediction_text='Loan eligiblity is {}'.format(output))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
